870b5f7984776629ee06524091b48eab343145fd
Gabriel <Gabriel@DESKTOP-18KFKCB>
2024-07-17 01:03:48 +0200

    Dodaj pliki projektów.

---
+++ ScpEconomy/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using PluginAPI.Core.Attributes;
+
+namespace ScpEconomy
+{
+    public class Plugin
+    {
+        public const string PluginVersion = "0.0.0";
+
+        public static Plugin Instance { get; private set; }
+
+        [PluginEntryPoint("ScpEconomy", PluginVersion, "Plugin that adds economy to your SCP:SL server.", "w36131")]
+        public void OnLoad()
+        {
+            Instance = this;
+        }
+
+        public void OnUnload()
+        {
+            Instance = null;
+        }
+    }
+}
