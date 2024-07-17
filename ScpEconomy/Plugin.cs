950001cb4e649b5589a0823987ece9f1d87756ef
w36131 <gswidnik@proton.me>
2024-07-17 22:06:45 +0200

    Added the PlayerData directory and PlayerJoined event and also IsEnabled config option functionality.

--- ScpEconomy/Plugin.cs
+++ ScpEconomy/Plugin.cs
@@ -1,5 +1,11 @@
-﻿using PluginAPI.Core;
+﻿using PluginAPI.Enums;
 using PluginAPI.Core.Attributes;
+using PluginAPI.Helpers;
+using System;
+using System.IO;
+using System.Reflection;
+using PluginAPI.Events;
+using PluginAPI.Core;
 
 namespace ScpEconomy
 {
@@ -12,17 +18,40 @@ namespace ScpEconomy
         [PluginConfig]
         public Config Config;
 
+        public string PlayerDataDirectory { get; private set; }
+
         [PluginEntryPoint("ScpEconomy", PluginVersion, "Plugin that adds economy to your SCP:SL server.", "w36131")]
         public void OnLoad()
         {
+            if (!Config.IsEnabled)
+                return;
+
             Instance = this;
 
-            ServerConsole.AddLog($"ScpEconomy [{PluginVersion}] has been loaded.");
+            ServerConsole.AddLog($"ScpEconomy-{PluginVersion} has been loaded.\nBy w36131", ConsoleColor.Green);
+
+            PlayerDataDirectory = Path.Combine(Path.GetDirectoryName(PluginHandler.Get(this).MainConfigPath), "PlayerData");
+
+            if (!Directory.Exists(PlayerDataDirectory))
+            {
+                Directory.CreateDirectory(PlayerDataDirectory);
+            }
+
+            EventManager.RegisterAllEvents(this);
         }
 
+        [PluginUnload]
         public void OnUnload()
         {
             Instance = null;
+
+            EventManager.UnregisterAllEvents(this);
+        }
+
+        [PluginEvent(ServerEventType.PlayerJoined)]
+        public void OnPlayerJoined(PlayerJoinedEvent ev)
+        {
+
         }
     }
 }
