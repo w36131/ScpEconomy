647f73bfb9b8437de62fc0bb81399c343f41946a
w36131 <gswidnik@proton.me>
2024-07-18 10:22:51 +0200

    Reworked DataManagement, Added new commands and modified other things.

---
+++ ScpEconomy/API/DataManagement/ItemShop.cs
@@ -0,0 +1,34 @@
+﻿using ScpEconomy.API.DataObjects;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScpEconomy.API.DataManagement
+{
+    public class ItemShop
+    {
+        public static List<VirtualItem> GetAllVirtualItems()
+        {
+            var items = new List<VirtualItem>();
+
+            if (Plugin.Instance.Config.ItemShop.Count == 0)
+                return null;
+
+            foreach(var virtualItem in Plugin.Instance.Config.ItemShop)
+            {
+                if (!VirtualItem.RegisteredVirtualItems.Any(x => x.Name == virtualItem))
+                    continue;
+
+                items.Add(VirtualItem.RegisteredVirtualItems.FirstOrDefault(x => x.Name == virtualItem));
+            }
+
+            return items;
+        }
+        public static VirtualItem GetVirtualItem(string virtualItemName)
+        {
+            if (!VirtualItem.RegisteredVirtualItems.Any(x => x.Name == virtualItemName))
+                return null;
+
+            return VirtualItem.RegisteredVirtualItems.FirstOrDefault(x => x.Name == virtualItemName);
+        }
+    }
+}
--- ScpEconomy/API/DataObjects/VirtualItem.cs
+++ ScpEconomy/API/DataObjects/VirtualItem.cs
@@ -7,32 +7,13 @@ namespace ScpEconomy.API.DataObjects
 {
     public abstract class VirtualItem
     {
-        [NonSerialized()]
-        public static List<VirtualItem> RegisteredVirtualItems = new();
+        public VirtualItem() => RegisteredVirtualItems.Add(this);
+
+        public static HashSet<VirtualItem> RegisteredVirtualItems = new();
 
         public abstract string Name { get; set; }
         public virtual string Description { get; set; } = "No description provided.";
         public virtual int Price { get; set; } = 0;
-        public virtual Color ItemColor { get; set; } = Color.white;
-        public virtual void Purchase(string userId)
-        {
-            if(DataManagement.GetPlayerData(userId) != null)
-                if(DataManagement.GetPlayerData(userId).Credits >= Price)
-                {
-                    DataManagement.RemoveCredits(userId, Price);
-                    OnPurchased(userId, Price);
-                }
-        }
-        public virtual void Purchase(Player player)
-        {
-            if (DataManagement.GetPlayerData(player) != null)
-                if (DataManagement.GetPlayerData(player).Credits >= Price)
-                {
-                    DataManagement.RemoveCredits(player, Price);
-                    OnPurchased(player, Price);
-                }
-        }
-        public abstract void OnPurchased(string userId, int virtualItemPrice);
-        public abstract void OnPurchased(Player player, int virtualItemPrice);
+        public virtual Color VirtualItemColor { get; set; } = Color.white;
     }
 }
